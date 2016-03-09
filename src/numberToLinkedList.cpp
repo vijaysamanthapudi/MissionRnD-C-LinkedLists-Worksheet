458c45fde4b736ccaa19b07ce8a6dcbc91a7f7c3
vijaysamanthapudi <vijaysamanthapudi@missionrnd.com>
2016-03-09 06:13:52 +0530

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -13,12 +13,45 @@ NOTES: For negative numbers ignore negative sign.
 
 #include <stdio.h>
 #include <malloc.h>
-
+struct node *create_node(struct node *head, int num);
 struct node {
 	int num;
 	struct node *next;
 };
 
-struct node * numberToLinkedList(int N) {
-	return NULL;
+struct node * numberToLinkedList(int N)
+{
+	struct node *head=NULL;
+	if (N < 0)
+	{
+		N = -1*N;
+	}
+	if (N == 0)
+	{
+		head = create_node(head, N);
+	}
+	
+
+	while (N)
+	{
+		head=create_node(head,N % 10);
+		N /= 10;
+
+	}
+	
+	return head;
+}
+
+
+
+struct node *create_node(struct node*head, int num)
+{
+
+	struct node *h = (struct node*)malloc(sizeof(struct node));
+	h->num = num;
+	h->next = head;
+	head = h;
+	return head;
+
+
 }
