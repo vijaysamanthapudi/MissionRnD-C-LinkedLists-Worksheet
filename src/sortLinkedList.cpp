c5f8e5ccb22a3ae1c72254642c2c352567d0ae05
vijaysamanthapudi <vijaysamanthapudi@missionrnd.com>
2016-03-11 22:38:59 +0530

    Submit Commit

--- src/sortLinkedList.cpp
+++ src/sortLinkedList.cpp
@@ -1,4 +1,4 @@
-/*
+ /*
 OVERVIEW: Given an unsorted single linked list, sort the linked list without using extra array or linked list.
 E.g.: 4->2->1->5->3, output is 1->2->3->4->5.
 
@@ -18,6 +18,30 @@ struct node {
 	struct node *next;
 };
 
-struct node * sortLinkedList(struct node *head) {
-	return NULL;
+struct node * sortLinkedList(struct node *head)
+{
+	if (head == NULL)
+	{
+		return NULL;
+	}
+
+	struct node *prev = head, *curr = head->next;
+	while (curr)
+	{
+		prev = head;
+		while (prev)
+		{
+			if (prev->num > curr->num)
+			{
+				int temp = prev->num;
+				prev->num = curr->num;
+				curr->num = temp;
+			}
+			prev = prev->next;
+
+		}
+		curr = curr->next;
+	}
+	return head;
+	
 }
